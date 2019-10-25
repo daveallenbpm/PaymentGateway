f6a248221aa4db60d49e00dc1fa27aa2c1e60fd2
Dave Allen <daveallenbpm@gmail.com>
2019-10-25 13:47:52 +0100

    Fix sqlite database creation

--- Setup.ps1
+++ Setup.ps1
@@ -1,29 +1,24 @@
-
 # Checks whether a given command has been installed
 function Check-Installed($command) {
 	return (Get-Command $command -ErrorAction SilentlyContinue)
 }
 
-# Install chocolatey if not installed
-if (!Check-Installed("choco")) {
-	# Install chocolatey and refresh path
-	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-	$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-}
+if (!(Check-Installed("sqlite3"))) {
+	if (!(Check-Installed("choco"))) {
+		# Install chocolatey and refresh path
+		Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+		$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+	}
 
-if (!Check-Installed("sqlite3")) {
 	choco install sqlite
 }
 
-
-if (![System.IO.File]::Exists(".\PaymentGateway\Database\Database.db")) {
-	echo "Installing database"
-	sqlite3 .\PaymentGateway\Database\Database.db
-	.exit
+if (!([System.IO.File]::Exists('.\PaymentGateway\Database\Database.db'))) {
+	echo "Creating database"
+	sqlite3 .\PaymentGateway\Database\Database.db "VACUUM;"
 }
 
-# Install ef if not already there.
-if (!Check-Installed("dotnet-ef")) {
+if (!(Check-Installed("dotnet-ef"))) {
 	dotnet tool install --global dotnet-ef
 }
 
