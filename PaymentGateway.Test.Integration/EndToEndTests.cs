1cd07788a5ebb81582c8c0d7762fc93e66a9afaa
Dave Allen <daveallenbpm@gmail.com>
2019-10-25 17:06:57 +0100

    Add an end-to-end test.

---
+++ PaymentGateway.Test.Integration/EndToEndTests.cs
@@ -0,0 +1,55 @@
+﻿using PaymentGateway.Enums;
+using PaymentGateway.ExternalServices;
+using PaymentGateway.Models;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace PaymentGateway.Test.Integration
+{
+    public class EndToEndTests
+    {
+        [Fact]
+        public async void Submit_a_valid_payment_and_check_it_exists_in_the_database()
+        {
+            // Todo - needs url
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44355");
+
+            var request = new PaymentPostDto
+            {
+                CardNumber = "5555555555554444",
+                ExpiryDate = DateTime.Now.AddDays(10),
+                Amount = 10,
+                Currency = "GBP",
+                CVV = "123"
+            };
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+            var postResponse = await client.PostAsync("/api/payment", content);
+
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+            var responseData = JsonSerializer.Deserialize<BankResponse>(responseString, jsonOptions);
+
+
+            var getResponse = await client.GetAsync($"/api/payment/paymentId/{responseData.PaymentId}");
+            var getResponseString = await getResponse.Content.ReadAsStringAsync();
+            var getResponseData = JsonSerializer.Deserialize<PaymentGateway.Test.Integration.PaymentGetDto>(getResponseString, jsonOptions);
+
+            Assert.Equal(responseData.PaymentId, getResponseData.PaymentId);
+            Assert.Equal(PaymentStatus.Success.ToString(), getResponseData.PaymentStatus);
+            Assert.Equal("************4444", getResponseData.CardNumber);
+            Assert.Equal(request.ExpiryDate.Date, getResponseData.ExpiryDate);
+            Assert.Equal(10, getResponseData.Amount);
+            Assert.Equal("GBP", getResponseData.Currency);
+            Assert.Equal("123", getResponseData.CVV);
+        }
+    }
+}
---
+++ PaymentGateway.Test.Integration/PaymentGetDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PaymentGateway.Controllers;
+
+namespace PaymentGateway.Test.Integration
+{
+    public class PaymentGetDto
+    {
+        public int Id { get; set; }
+        public Guid PaymentId { get; set; }
+        public string PaymentStatus { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string CVV { get; set; }
+    }
+}
