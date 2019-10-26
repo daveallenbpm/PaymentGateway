55b89e9071ae80029c7672fe2440641463f089fe
Dave Allen <daveallenbpm@gmail.com>
2019-10-26 11:03:58 +0100

    Add some additional restrictions on the database

--- PaymentGateway/DataAccess/AppDbContext.cs
+++ PaymentGateway/DataAccess/AppDbContext.cs
@@ -11,5 +11,23 @@ namespace PaymentGateway.DataAccess
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<PaymentEntity> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<PaymentEntity>()
+                .HasIndex(p => p.PaymentId)
+                .IsUnique();
+
+            modelBuilder
+                .Entity<PaymentEntity>()
+                .Property(p => p.CVV)
+                .HasMaxLength(3);
+
+            modelBuilder
+                .Entity<PaymentEntity>()
+                .Property(p => p.Currency)
+                .HasMaxLength(3);
+        }
     }
 }
---
+++ PaymentGateway/Migrations/20191026100259_ColumnRestrictions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentGateway.Migrations
+{
+    public partial class ColumnRestrictions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_PaymentId",
+                table: "Payments",
+                column: "PaymentId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_PaymentId",
+                table: "Payments");
+        }
+    }
+}
